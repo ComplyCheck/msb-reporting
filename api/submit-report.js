b0a05d6a1c8a73ee008ce38bc78d094a9084e5f6
Philip Laufman <phil@complycheck.co>
2025-06-14 09:41:53 -0400

    Update submit-report.js

--- api/submit-report.js
+++ api/submit-report.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
     } = req.body;
 
     // Create email transporter
-    const transporter = nodemailer.createTransporter({
+    const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
         user: process.env.EMAIL_USER,
